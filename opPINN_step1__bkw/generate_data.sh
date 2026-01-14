b460ae2e24c7fe319fcc0bc8532cfd8d521fa3b4
GJ2-lab <rhkdwo2048@gmail.com>
2026-01-14 12:41:57 +0900

    260114-02

--- opPINN_step1__bkw/generate_data.sh
+++
@@ -1,6 +0,0 @@
-gamma=0.0
-echo "Generating a dataset for gamma: $gamma (Maxwellian molecules only)"
-# Train the operator D and F
-nohup python generate_dataset.py > data_generation__2d.log &
-wait
-echo "All jobs submitted!"
